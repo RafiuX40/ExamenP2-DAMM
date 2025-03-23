d11554ae375ebd5e0e15c3ebf2745a498bff08b9
Rafael de Jesús Ramos Chávez <22090@portalcedi.edu.mx>
2025-03-23 14:46:10 -0600

    Acomodo e integracion de Firebase al proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzMui6-KtEmIk6LmB8PhkqCYJWLJNrRN4",
+  authDomain: "examenp2damm.firebaseapp.com",
+  projectId: "examenp2damm",
+  storageBucket: "examenp2damm.firebasestorage.app",
+  messagingSenderId: "58972041734",
+  appId: "1:58972041734:web:34dffad9d297280fe1c987"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
