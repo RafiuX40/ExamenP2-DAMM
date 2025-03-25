a14afeb041a9f5ff11e4e92c6ee19c57cfc64ab3
MarianaC9910 <21152@portalcedi.edu.mx>
2025-03-25 08:31:26 -0600

    reset

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule],
 })
-export class ResetPasswordPage implements OnInit {
+export class ResetPasswordPage {
+  email: string = '';
 
-  constructor() { }
+  constructor(private firebaseService: AuthService) {}
 
-  ngOnInit() {
+  async resetPassword() {
+    try {
+      const message = await this.firebaseService.resetPassword(this.email);
+      alert(message);
+    } catch (error) {
+      alert('Error: ' + (error instanceof Error ? error.message : 'An unknown error occurred'));
+    }
   }
-
 }
