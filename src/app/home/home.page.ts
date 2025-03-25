737d6c51cd391468ab571f85e294d9c9071d649f
Rafael de Jesús Ramos Chávez <22090@portalcedi.edu.mx>
2025-03-25 09:43:47 -0600

    POrque estaba el openAI en el home xD?

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,9 +5,8 @@ import { Router } from '@angular/router';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonText, IonCardTitle, IonCard, IonCardContent, IonCardHeader, IonLabel, IonItem } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { OpenaiService } from '../openai.service';
 import { RouterModule } from '@angular/router';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../services/auth.service';
 import { AlertController } from '@ionic/angular'
 @Component({
   selector: 'app-home',
@@ -17,23 +16,14 @@ import { AlertController } from '@ionic/angular'
   imports: [RouterModule, IonContent, IonLabel, IonHeader, IonCardTitle, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonInput, IonText, IonCard, IonCardContent, IonCardHeader, IonItem, IonCardTitle]
 })
 export class HomePage {
-  constructor(private router: Router, private openAIService: OpenaiService, private authService: AuthService, private alertController: AlertController) {}
+  constructor(private router: Router, private authService: AuthService, private alertController: AlertController) {}
 
   ideaPrompt: string = '';
   generatedIdea: string = '';
 
-
-  async generateIdea(){
-    if(this.ideaPrompt.trim() === ''){
-      alert('Por favor ingresa una idea inicial.');
-      return;
-    }
-    this.generatedIdea = await this.openAIService.generateIdea(this.ideaPrompt);
-  }
-
   async logout() {
     try{
-      await this.authService.logout();
+      await this.authService.logOut();
       const alert = await this.alertController.create({
         header: 'signout Success',
         message: 'You had logout succesfully!',
