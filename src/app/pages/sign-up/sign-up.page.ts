c855f5137dba62389607781d3d2e8f789999849a
MarianaC9910 <21152@portalcedi.edu.mx>
2025-03-25 08:27:38 -0600

    signupavance

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,20 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCardTitle } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular'
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [RouterModule, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCardTitle]
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
 
   ngOnInit() {
   }
+  email: string = '';
+  password: string = '';
 
+  constructor(
+    private authService: AuthService, 
+    private alertController: AlertController,
+    private router: Router) { }
+
+  async onSubmit() {
+    try{
+      await this.authService.register(this.email, this.password);
+      const alert = await this.alertController.create({
+        header: 'Signup Success',
+        message: 'Your account has been created succesfully!',
+        buttons: ['Ok'],
+      });
+      await alert.present();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'An error ocurred during signup.',
+        buttons: ['Ok'],
+      });
+      await alert.present();
+    }
+
+  }
+
+
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+
+  onSignUp() {
+    this.router.navigateByUrl("login")
+  }
 }
+
+
